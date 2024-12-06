b474cf7baf5c9cad8cfff44618be93c6db08a587
Pritam Dhaugoda <pritamdhaugoda@gmail.com>
2024-12-06 16:09:25 +0545

    fix build error

--- src/Components/Recipe/Video.js
+++ src/Components/Recipe/Video.js
@@ -2,12 +2,10 @@ import React from "react";
 
 const Video = ({ items }) => {
   let embedUrl = "";
-  const embedYouTubeVideo = () => {
-    const videoUrl = items[0].strYoutube;
-    const videoId = videoUrl.split("v=")[1];
-    embedUrl = `https://www.youtube.com/embed/${videoId}`;
-  };
-  embedYouTubeVideo();
+  const videoUrl = items[0].strYoutube;
+  const videoId = videoUrl.split("v=")[1];
+  embedUrl = `https://www.youtube.com/embed/${videoId}`;
+
   return (
     <>
       <h5 className="mt-4">Video:</h5>
@@ -17,6 +15,7 @@ const Video = ({ items }) => {
           src={embedUrl}
           allowFullScreen
           style={{ width: "100%", height: "500px" }}
+          title={`Video: ${videoId}`}
         ></iframe>
       </div>
     </>
